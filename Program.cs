efb8153a9195511d1fe8937a1bb4b9e0c4595eab
sidqi <sidqiarasyid31@gmail.com>
2025-04-08 07:56:03 +0700

    menambahkan class DataMahasiswa103022300108

--- Program.cs
+++ Program.cs
@@ -1,3 +1,9 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+public class main
+{
+    public static void Main(string[] args)
+    {
+        DataMahasiswa103022300108.ReadJSON();
+    }
+}
 //
